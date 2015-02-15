9ec9711136a81bbecd044a5b03056a24eda37e12
andrushm <andrush@ukr.net>
2015-02-15 18:45:20 +0200

    Add AppController

--- index.php
+++ index.php
@@ -12,17 +12,50 @@ class RouterFactory
     }
 }
 
+class AppController
+{
+    public $respons = null;
 
-class MainController
+    public function __construct(){
+
+    }
+
+    public function run(){
+        $this->respons = 'respons';
+        $url = $_SERVER['REQUEST_URI'].'/';
+        $url_param = array();
+        preg_match_all('([^\/]+)', $url, $url_param); // ([^\/]+)  ([*/]\w+)
+        $url_param = $url_param[0];
+        //var_dump($url_param);
+        $controller = $url_param[1];
+        $action = $url_param[2];
+        $product = RouterFactory::build($controller);
+        var_dump($product->getRespons());
+        echo 'Controller:'.$controller.'<br>action:'.$product->{$action}();
+    }
+
+}
+
+class MainController extends AppController
 {
     private $name = 'Table';
+    public $respons = null;
+
+    public function __construct(){
+
+    }
+
     public function getName()
     {
         return $this->name;
     }
+
+    public function getRespons(){
+        return $this->respons;
+    }
 }
 
-class SecondController
+class SecondController extends AppController
 {
     private $name = 'Phone';
     public function getName()
@@ -31,14 +64,9 @@ class SecondController
     }
 }
 
-$url = $_SERVER['REQUEST_URI'].'/';
-$url_param = array();
-preg_match_all('([^\/]+)', $url, $url_param); // ([^\/]+)  ([*/]\w+)
-$url_param = $url_param[0];
-//var_dump($url_param);
-$controller = $url_param[1];
-$action = $url_param[2];
-$product = RouterFactory::build($controller);
-echo 'Controller:'.$controller.'<br>action:'.$product->{$action}();
+$main = new AppController();
+$main->run();
+
+
 
 ?>
